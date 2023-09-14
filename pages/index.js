f6638c1acd227838e5d0cf6b2fbad0ad35c8e7b5
Ivan Garcia <141492899+ivancsgarcia@users.noreply.github.com>
2023-09-14 11:53:52 +0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>10 Things That Require Zero Talent</p>
+        <h2>10 Things That Require Zero Talent</h2>
     <ul>
       <li>1. Being on Time</li>
     <li>2. Making An Effort</li>
